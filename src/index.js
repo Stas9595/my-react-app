f053c4debb9cf6a659f66cee4e6ad5a33559e6ca
stasg <stas.grigorevskiy@gmail.com>
2019-07-22 16:35:27 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import {render} from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Article() {
+    const body = <section>body</section>
+    return (
+        <div className="App">
+            <p>Hello</p>
+            {body}
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+    return (
+        <div>
+            <h1>App name</h1>
+            <Article/>
+        </div>
+    )
+}
+
+render(<App/>, document.getElementById('root'));
